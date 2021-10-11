b535fcb80f6c9523bb8c33f80b98be578194e410
Odeh <abuzaid163@hotmail.com>
2021-10-11 19:02:57 +0300

    sum_series

--- math_series/series.py
+++ math_series/series.py
@@ -6,3 +6,8 @@ def lucas(n: int):
     return (n - 1) + (n - 1)
 
 
+def sum_series(n: int, first: int = 0, second: int = 1):
+    if first == 0 and second == 1:
+        return fibonacci(n)
+    elif first == 2 and second == 1:
+        return lucas(n)
--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,9 +1,9 @@
 from math_series import __version__
-from math_series.series import fibonacci, lucas
+from math_series.series import fibonacci, lucas, sum_series
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
 
 
 def test_fibonacci(number: int):
@@ -12,11 +12,7 @@ def test_fibonacci(number: int):
     elif number == 1 or number == 2:
         assert fibonacci(number) == 1
     elif number >= 3:
-        assert fibonacci(number) == (number - 1)
-
-
-
-
+        assert fibonacci(number) == number - 1
 
 
 def test_lucas(number: int):
@@ -25,11 +21,18 @@ def test_lucas(number: int):
     elif number == 1 or number == 2:
         assert lucas(number) == 1
     elif number >= 3:
-        assert lucas(number) == (number - 1)
+        assert lucas(number) == number - 1
 
+
+def test_sum_series(number: int, f: int = 1, s: int = 2):
+    if f == 1 and s == 2:
+        assert sum_series(number) == fibonacci(number)
+    elif f == 2 and s == 1:
+        assert sum_series(number) == lucas(number)
 
 
 for i in range(0, 11):
     test_fibonacci(i)
     test_lucas(i)
-    
+    test_sum_series(i)
+    test_sum_series(i, 2, 1)

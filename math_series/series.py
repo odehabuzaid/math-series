54f7790271d109efad965679001ab5eed8d05834
Odeh <abuzaid163@hotmail.com>
2021-10-11 18:47:16 +0300

    fibonacci

---
+++ math_series/series.py
@@ -0,0 +1,5 @@
+def fibonacci(n: int):
+    return (n - 1) + (n - 2)
+
+
+
--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,5 +1,22 @@
 from math_series import __version__
+from math_series.series import fibonacci
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_fibonacci(number: int):
+    if number == 0:
+        assert fibonacci(number) == 0
+    elif number == 1 or number == 2:
+        assert fibonacci(number) == 1
+    elif number >= 3:
+        assert fibonacci(number) == (number - 1)
+
+
+
+for i in range(0, 11):
+    test_fibonacci(i)
+
+

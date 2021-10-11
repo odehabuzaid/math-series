74712463cc06cde0f28ca0001e984c714cb4a50b
Odeh <abuzaid163@hotmail.com>
2021-10-11 19:50:09 +0300

    fibonacci 5-test-pass

--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -6,12 +6,17 @@ def test_version():
     assert __version__ == "0.1.0"
 
 
-def test_fibonacci():
-    """
-    Test the fibonacci function
-    """
-    assert str(fibonacci(0)) == '0'
+def test_fibonacci0():
+    assert fibonacci(0) == 0
 
+def test_fibonacci1():
+    assert fibonacci(1) == 1
+
+def test_fibonacci2():
+    assert fibonacci(2) == 1
+
+def test_fibonacci3():
+    assert fibonacci(3) == 2
 
 
 
@@ -23,14 +28,13 @@ def test_fibonacci():
 #     elif n == 2:
 #         assert lucas(n) == 3
 #     elif n == 3:
-#         assert lucas(n) == 4   
+#         assert lucas(n) == 4
 #     elif n == 4:
 #         assert lucas(n) == 7
 #     elif n == 5:
 #         assert lucas(n) == 11
 #     elif n == 6:
 #         assert lucas(n) == 18
-    
 
 
 # def test_sum_series(n: int, f: int = 1, s: int = 2):
@@ -38,6 +42,3 @@ def test_fibonacci():
 #         assert sum_series(n) == fibonacci(n)
 #     elif f == 2 and s == 1:
 #         assert sum_series(n) == lucas(n)
-
-
-

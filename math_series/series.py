7a8352e4ec024a7dcddf274439268a981e5b7229
Odeh <abuzaid163@hotmail.com>
2021-10-11 20:02:08 +0300

    final solution

--- math_series/series.py
+++ math_series/series.py
@@ -2,7 +2,7 @@ def fibonacci(n: int):
     """
     takes n as integer and Returns the nth number in the Fibonacci sequence.
     if the number is less than or equal to 1, return the number.
-    if the number is greater than 1, return the sum of the two previous numbers in the sequence.
+    if the number is greater than 1, return the nth number in the Fibonacci sequence.
     """
     return n if n <= 1 else fibonacci(n - 1) + fibonacci(n - 2)
 
@@ -11,19 +11,16 @@ def lucas(n: int):
     """
     takes n as integer and Returns the nth number in the Fibonacci sequence.
     if the number is less than or equal to 0, return 2.
-    if the number equal or greater than 1, return the sum of the two previous numbers in the sequence.
+    if the number equal or greater than 1, return nth number in the lucas sequence.
     """
     return 2 if n == 0 else fibonacci(n + 1) + fibonacci(n - 1)
 
 
-print(
-    "\n".join([f"{(lucas(i),i)}" for i in range(7)]),
-)
-print()
-print(
-    "\n".join([f"{(fibonacci(i),i)}" for i in range(7)]),
-)
-
 
 def sum_series(n: int, first: int = 0, second: int = 1):
+    """
+    takes n as integer and Returns :
+    nth number in the Fibonacci sequence. if the first argument is 0 and second argument is 1,
+    nth number in the lucas sequence. if the first argument is 2 and second argument is 1,
+    """
     return fibonacci(n) if first == 0 and second == 1 else lucas(n)
